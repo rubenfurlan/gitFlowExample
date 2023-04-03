0b563771b5bf4749b1aedf75a02058422b055625
rubenfurlan <ruben.furlan@despegar.com>
2023-04-03 17:09:34 -0300

    first commit

---
+++ src/main/java/com/despegar/git/flow/example/gitflowexample/GitFlowExampleApplication.java
@@ -0,0 +1,21 @@
+package com.despegar.git.flow.example.gitflowexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitFlowExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowExampleApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/test/java/com/despegar/git/flow/example/gitflowexample/GitFlowExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despegar.git.flow.example.gitflowexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
